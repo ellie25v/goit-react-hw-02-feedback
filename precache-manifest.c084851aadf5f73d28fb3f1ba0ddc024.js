cc7e5f4675ca9442031ee01130e45794687c7e9e
ellie25v <yelizarieva.ellie@gmail.com>
2020-03-11 15:38:22 +0200

    Updates

---
+++ precache-manifest.c084851aadf5f73d28fb3f1ba0ddc024.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce40783f261584c159daa408e7917f5",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "a8cf320d68c9feef73e1",
+    "url": "/goit-react-hw-02-feedback/static/js/2.15da36ea.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.15da36ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25c44572af3ec58c2037",
+    "url": "/goit-react-hw-02-feedback/static/js/main.79166f5f.chunk.js"
+  },
+  {
+    "revision": "df69af9126d73b466e5d",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.b5da2866.js"
+  }
+]);
